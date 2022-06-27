a9922fb30118ad0e38ba2bac0a7ab30928987ff3
yofikingstonp111 <yofikingston@gmail.com>
2022-06-27 16:18:13 +0700

    flask_mega_rijal

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id',db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id',db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,8 +18,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
-
-
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
