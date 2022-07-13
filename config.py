16091c89a68e66498a110cde26dbf27bad742f58
yofikingstonp111 <yofikingston@gmail.com>
2022-07-13 22:59:37 +0700

    flask_mega_rijal

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['rijal.ortonrko90900@gmail.com']
+    ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
 
