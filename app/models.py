1239ca72a35aeec4b934e3deab72406b226642e6
yofikingstonp111 <yofikingston@gmail.com>
2022-07-17 01:10:04 +0700

    flask_mega_rijal1

--- app/models.py
+++ app/models.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 from hashlib import md5
+from time import time
+import jwt
+from app import app
+
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+import app
 from app import db, login
 
 followers = db.Table('followers',
@@ -55,6 +62,21 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(self.posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password' : self.id, 'exp' : time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
 @login.user_loader
 def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,10 @@ from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, EmptyForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
+
 
 
 @app.before_request
@@ -89,6 +91,21 @@ def register():
         flash('Congratulation, you are now a registered user')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+            flash('Check your email for the instructions to reset your password')
+            return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
 
 @app.route('/user/<username>')
 @login_required
