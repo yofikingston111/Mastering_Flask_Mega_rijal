b5e0bbc13ebccee0f69e391be3902ff040758c71
yofikingstonp111 <yofikingston@gmail.com>
2022-08-09 23:21:19 +0700

    flask_mega_rijal1

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ from datetime import datetime
 from hashlib import md5
 from time import time
 from app import app
-from flask import current_app, url_for
+from flask import current_app
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-import app
+
 from app import db, login
 
 
@@ -66,12 +66,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password' : self.id, 'exp' : time() + expires_in},
-            current_app.config['SECRET_KEY'], algorithm='HS256')
+            app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+            id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
